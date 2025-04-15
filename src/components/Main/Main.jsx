23bd3bce666ca50b8c2375b0098f4898be78ed82
Everest Paudel <paudeleverest09@gmail.com>
2025-04-16 00:17:47 +0545

    Feat: enhance Main component with new search box and card image updates

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -22,12 +22,12 @@ const Main = () => {
 
                 <div className="card">
                     <p>Briefly Summarize this concept</p>
-                    <img src={assets.compass_icon} alt="" />
+                    <img src={assets.bulb_icon} alt="" />
                 </div>
 
                 <div className="card">
                     <p>BrainStorm team building</p>
-                    <img src={assets.bulb_icon} alt="" />
+                    <img src={assets.message_icon} alt="" />
                 </div>
 
                 <div className="card">
@@ -35,7 +35,23 @@ const Main = () => {
                     <img src={assets.code_icon} alt="" />
                 </div>
             </div>
+            <div className="main-bottom">
+                <div className="search-box">
+                    <input type="text" placeholder='Enter Promt Here'/>
+                    <div>
+                <img src={assets.gallery_icon} alt="" />
+                <img src={assets.mic_icon} alt="" />
+                <img src={assets.send_icon} alt="" />
+                </div>
+               
+                </div>
+                <p className='bottom-info'>
+Juhi may be wrong  please verify the information provided by Juhi.
+            </p>
+            </div>
+          
         </div>
+
     </div>
   )
 }
